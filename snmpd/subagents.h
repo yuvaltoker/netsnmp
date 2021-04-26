ca2c1d49b3e42ea49970e5288d230700fe44afa6
yuvaltoker <yuvalt2710@gmail.com>
2021-04-26 16:34:13 +0300

    start remaking previous modem-ra-project for current use

---
+++ snmpd/subagents.h
@@ -0,0 +1,15 @@
+#ifndef SUBAGENTS_H
+#define SUBAGENTS_H
+
+/* function declaration */
+void connectToRedis(void);
+void shutdownRedis(void);
+void init_subagents(void);
+Netsnmp_Node_Handler handle_batteryObject;
+Netsnmp_Node_Handler handle_channelObject;
+Netsnmp_Node_Handler handle_currentObject;
+Netsnmp_Node_Handler handle_isAliveObject;
+void GET_objects_redis(int mode);
+void SET_objects_redis(int mode);
+#endif /* SUBAGENTS_H */
+
