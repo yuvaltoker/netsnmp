ceec0d7c4f17d330e26f28bcb928cd1eb2e526e1
yuvaltoker <yuvalt2710@gmail.com>
2021-05-02 16:55:10 +0300

    doesnt work yet, snmpwalk in snmpd itself with MY-MIB::my.... doesnt work, check it first

--- sender/send-snmp.py
+++ sender/send-snmp.py
@@ -1,6 +1,19 @@
 import netsnmp
 
-var = netsnmp.Varbind('1.3.6.1.4.1.8073.1.1.4', 0)
-res = netsnmp.snmpget(var, Version=2, DestHost="snmpd:1662",  Community="public")
-print(res[0])
+var = netsnmp.VarList('MY-MIB::myIntMibObject', 'MY-MIB::myStringMibObject')
+res = netsnmp.snmpwalk(var, Version=2, DestHost="snmpd:1662",  Community="public")
+#res = netsnmp.snmpset(var, Version=2, DestHost="snmpd:1662",  Community="public")
+#print(res[0])
 #print(res[0]*2)
+
+
+#serv = "snmpd:1662"
+#snmp_pass = "public"
+
+#oid = netsnmp.VarList('IF-MIB::ifName','IF-MIB::ifDescr')
+#oid = netsnmp.VarList('IF-MIB::ifHCInOctets','IF-MIB::ifDescr')
+#snmp_res = netsnmp.snmpwalk(oid, Version=2, DestHost=serv, Community=snmp_pass)
+for x in res:
+    print("snmp_res:: ", x)
+
+#snmpwalk -c public -v2c localhost:1662 IF-MIB::ifHCInOctets
