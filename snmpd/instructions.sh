e55e4ebcd79746332207a53d5aa88286f2883c55
yuvaltoker <yuvalt2710@gmail.com>
2021-05-04 17:30:47 +0300

    changes were made in order to get v3 snmpget and snmpset, yet to succeed

--- snmpd/instructions.sh
+++ snmpd/instructions.sh
@@ -4,7 +4,11 @@
 echo "snmpd:127.0.0.1" >> /etc/hosts.allow
 echo "master agentx" >> /etc/snmp/snmpd.conf
 
-snmpd -f -Lf snmpd_file.log -C -M /usr/share/snmp/mibs --rwcommunity=public --master=agentx --agentXSocket=tcp:localhost:161 udp:1662 &
+# read hie code snippet in https://stackoverflow.com/questions/48684497/snmpget-unknown-user-name
+
+#snmpd -f -Lf snmpd_file.log -C -M /usr/share/snmp/mibs --rwcommunity=public --master=agentx --agentXSocket=tcp:localhost:161 udp:1662 &
+snmpd -f -Lf snmpd_file.log -C -M /usr/share/snmp/mibs --master=agentx --agentXSocket=tcp:localhost:161 udp:1662 &
+/usr/local/sbin/snmpd -c /var/net-snmp/snmpd.conf -c /usr/local/share/snmp/snmpd.conf -c /etc/snmp/snmpd.conf
 
 cd create_snmpd
 make example-daemon
