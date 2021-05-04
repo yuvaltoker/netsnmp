7130125b6a5e6abdf800d0a2d9aea8b3439d50df
yuvaltoker <yuvalt2710@gmail.com>
2021-05-04 12:18:46 +0300

    can get and set mib's value trhough python, v2 checked, yet to check v3

--- sender/send-snmp.py
+++ sender/send-snmp.py
@@ -1,27 +1,50 @@
 from easysnmp import Session
 
+# examles of using easysnmp modlue https://easysnmp.readthedocs.io/en/latest/
+
 # Create an SNMP session to be used for all our requests
 session = Session(hostname='snmpd:1662', community='public', version=2)
 
 # You may retrieve an individual OID using an SNMP GET
-location = session.get('sysLocation.0')
+myIntMibObject = session.get('1.3.6.1.4.1.8073.1.1.4')
+print ('{oid}.{oid_index} {snmp_type} = {value}'.format(
+        oid=myIntMibObject.oid,
+        oid_index=myIntMibObject.oid_index,
+        snmp_type=myIntMibObject.snmp_type,
+        value=myIntMibObject.value
+    ))
 
 # You may also specify the OID as a tuple (name, index)
 # Note: the index is specified as a string as it can be of other types than
 # just a regular integer
+'''
 contact = session.get(('sysContact', '0'))
-
+print ('{oid}.{oid_index} {snmp_type} = {value}'.format(
+        oid=contact.oid,
+        oid_index=contact.oid_index,
+        snmp_type=contact.snmp_type,
+        value=contact.value
+    ))
+'''
 # And of course, you may use the numeric OID too
-description = session.get('.1.3.6.1.2.1.1.1.0')
+#description = session.get('.1.3.6.1.2.1.1.1.0')
 
 # Set a variable using an SNMP SET
-session.set('sysLocation.0', 'The SNMP Lab')
+session.set('1.3.6.1.4.1.8073.1.1.5.0', 'its working just fine', 's')
+myStringMibObject = session.get('1.3.6.1.4.1.8073.1.1.5.0')
+print ('{oid}.{oid_index} {snmp_type} = {value}'.format(
+        oid=myStringMibObject.oid,
+        oid_index=myStringMibObject.oid_index,
+        snmp_type=myStringMibObject.snmp_type,
+        value=myStringMibObject.value
+    ))
 
 # Perform an SNMP walk
-system_items = session.walk('system')
+#system_items = session.walk('system')
 
 # Each returned item can be used normally as its related type (str or int)
 # but also has several extended attributes with SNMP-specific information
+'''
 for item in system_items:
     print ('{oid}.{oid_index} {snmp_type} = {value}'.format(
         oid=item.oid,
@@ -29,26 +52,4 @@ for item in system_items:
         snmp_type=item.snmp_type,
         value=item.value
     ))
-
-'''import netsnmp
-
-session = netsnmp.Session(Version=2, DestHost="snmpd:1662",  Community="public")
-
-var = netsnmp.VarList(netsnmp.Varbind('1.3.6.1.4.1.8073.1.1.4.0', 0))
-#var = netsnmp.VarList('MY-MIB::myIntMibObject')
-res = session.get(var)
-#print("res value: ", res)
-#print(res[0]*2)
-
-
-#serv = "snmpd:1662"
-#snmp_pass = "public"
-
-#oid = netsnmp.VarList('IF-MIB::ifName','IF-MIB::ifDescr')
-#oid = netsnmp.VarList('IF-MIB::ifHCInOctets','IF-MIB::ifDescr')
-#snmp_res = netsnmp.snmpwalk(oid, Version=2, DestHost=serv, Community=snmp_pass)
-for x in res:
-    print("snmp_res:: ", x)
-
-#snmpwalk -c public -v2c localhost:1662 IF-MIB::ifHCInOctets
 '''
