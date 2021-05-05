a797d13700f69d80bbe799e3dddc42f899ee1817
yuvaltoker <yuvalt2710@gmail.com>
2021-05-05 12:00:42 +0300

    added environments variables

--- sender/send-snmp.py
+++ sender/send-snmp.py
@@ -1,12 +1,39 @@
 from easysnmp import Session
+import os
+
+# Getting environment variables:
+# HOST
+# PORT
+# AUTH_PASS
+# PRIV_PASS
+
+class EnvVars:
+    def __init__(self):
+        self.host = os.getenv('HOST')
+        self.port = os.getenv('PORT')
+        self.auth_pass = os.getenv('AUTH_PASS')
+        self.priv_pass = os.getenv('PRIV_PASS')
+
+    def get_host(self):
+        return self.host
+
+    def get_port(self):
+        return self.port
+
+    def get_auth_pass(self):
+        return self.auth_pass
+
+    def get_priv_pass(self):
+        return self.priv_pass
 
 # examles of using easysnmp modlue https://easysnmp.readthedocs.io/en/latest/
 
+env_vars = EnvVars()
 # Create an SNMP session to be used for all our requests
-session = Session(hostname='snmpd:1662', version=3, 
-                  security_level='auth_with_privacy', security_username = 'mynewuser', 
-                  privacy_protocol = 'AES', privacy_password = 'yuval12345',
-                  auth_protocol = 'SHA', auth_password = 'yuval12321')
+session = Session(hostname = 'snmpd:1662', version = 3, 
+                  security_level = 'auth_with_privacy', security_username = 'mynewuser', 
+                  privacy_protocol = 'AES', privacy_password = env_vars.get_priv_pass(),
+                  auth_protocol = 'SHA', auth_password = env_vars.get_auth_pass())
 
 # You may retrieve an individual OID using an SNMP GET
 myIntMibObject = session.get('1.3.6.1.4.1.8073.1.1.4')
