e4bb9a5bffca57674ae9643678f598d07b730a75
yuvaltoker <yuvalt2710@gmail.com>
2021-05-05 17:37:42 +0300

    client is working with set and get requests from sam-server, can now build it as background proccess without volume

--- sender/send-snmp.py
+++ sender/send-snmp.py
@@ -1,4 +1,4 @@
-from easysnmp import Session
+import easysnmp
 from datetime import datetime
 import os
 import threading
@@ -20,6 +20,7 @@ class EnvVars:
         self.server_port = os.getenv('SERVER_PORT')
         self.auth_pass = os.getenv('AUTH_PASS')
         self.priv_pass = os.getenv('PRIV_PASS')
+        self.user_name = os.getenv('USER_NAME')
 
     def get_server_host(self):
         return self.server_host
@@ -33,18 +34,18 @@ class EnvVars:
     def get_priv_pass(self):
         return self.priv_pass
 
+    def get_user_name(self):
+        return self.user_name
+
 
 class Client(threading.Thread):
 
-    def __init__(self, env_vars, msg):
+    def __init__(self, env_vars, msg, sock):
         threading.Thread.__init__(self)
         self.env_vars = env_vars
         self.msg = msg
         self.json_string = ''
-        self.session = Session(version = 3, 
-                  security_level = 'auth_with_privacy', security_username = 'mynewuser', 
-                  privacy_protocol = 'AES', privacy_password = self.env_vars.get_priv_pass(),
-                  auth_protocol = 'SHA', auth_password = self.env_vars.get_auth_pass())
+        self.sock = sock
 
     def msg_to_json(self):
         request_json = json.loads(self.msg)
@@ -58,29 +59,34 @@ class Client(threading.Thread):
                               request_json['destination'])
         timestamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         self.json_string = ('{ "method": ' + request_json['method'] + '"' +
-                            ', "destination: ' + request_json['destination'] + '"' +
+                            ', "destination: "' + request_json['destination'] + '"' +
                             ', "oid": "' + request_json['oid'] + '"' +
                             ', "name": "' + request_json['name'] + '"' +
                             ', "type": "' + 'snmpResponse' + '"' +
                             ', "time": "' + timestamp + '"' +
                             ', "hash": "' + request_json['hash'] + '"' +
-                            ', "value": "' + value + '"}')
+                            ', "value": "' + value + '" }')
 
 
     def snmp_set(self, oid, value, data_type, host_name):
-        session.set(oid, value, data_type, hostname = host_name)
+        mib_object = easysnmp.snmp_set(oid, value, data_type, hostname = host_name, version = 3, 
+                  security_level = 'auth_with_privacy', security_username = self.env_vars.get_user_name(), 
+                  privacy_protocol = 'AES', privacy_password = self.env_vars.get_priv_pass(),
+                  auth_protocol = 'SHA', auth_password = self.env_vars.get_auth_pass())
 
-    def snmp_get(self, host_name):
-        mib_object = session.get(oid, hostname = host_name)
+    def snmp_get(self, oid, host_name):
+        mib_object = easysnmp.snmp_get(oid, hostname = host_name, version = 3, 
+                  security_level = 'auth_with_privacy', security_username = self.env_vars.get_user_name(), 
+                  privacy_protocol = 'AES', privacy_password = self.env_vars.get_priv_pass(),
+                  auth_protocol = 'SHA', auth_password = self.env_vars.get_auth_pass())
         return mib_object.value
 
     def send(self):
-        sock = socket.socket(socket.AF_INET, # Internet
-                      socket.SOCK_DGRAM) # UDP
-        sock.sendto(bytes(self.json_string, 'utf-8'), (self.env_vars.get_server_host, self.env_vars.get_server_port))
+        self.sock.send(bytes(self.json_string, 'utf-8'))
 
     def run(self):
         self.msg_to_json()
+        print('sending data: %s' % (self.json_string))
         self.send()
         #creating udp socket and sending the 
 
@@ -89,10 +95,11 @@ def main():
     buffer_size = 4096
     env_vars = EnvVars()
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('sam-server', '3044'))
+    sock.connect((env_vars.get_server_host(), int(env_vars.get_server_port())))
     while True:
-        data = s.recv(buffer_size)
-        cli = Client()
+        data = sock.recv(buffer_size)
+        print('data arrived: %s' % (str(data, 'utf-8')))
+        cli = Client(env_vars, str(data, 'utf-8'), sock)
         cli.start()
         
 
