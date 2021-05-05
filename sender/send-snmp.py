9133cf18bce1a2e90e49dc33f49197f24633003b
yuvaltoker <yuvalt2710@gmail.com>
2021-05-05 16:56:52 +0300

    client was added, in order to communicate with server of netsnmp requests

--- sender/send-snmp.py
+++ sender/send-snmp.py
@@ -1,24 +1,31 @@
 from easysnmp import Session
+from datetime import datetime
 import os
+import threading
+import json
+import socket
+
+# in class Client, the netsnmp part is using easysnmp module,
+# to learn more, enter the following link https://easysnmp.readthedocs.io/en/latest/
 
 # Getting environment variables:
-# HOST
-# PORT
+# SERVER_HOST
+# SERVER_PORT
 # AUTH_PASS
 # PRIV_PASS
 
 class EnvVars:
     def __init__(self):
-        self.host = os.getenv('HOST')
-        self.port = os.getenv('PORT')
+        self.server_host = os.getenv('SERVER_HOST')
+        self.server_port = os.getenv('SERVER_PORT')
         self.auth_pass = os.getenv('AUTH_PASS')
         self.priv_pass = os.getenv('PRIV_PASS')
 
-    def get_host(self):
-        return self.host
+    def get_server_host(self):
+        return self.server_host
 
-    def get_port(self):
-        return self.port
+    def get_server_port(self):
+        return self.server_port
 
     def get_auth_pass(self):
         return self.auth_pass
@@ -26,60 +33,70 @@ class EnvVars:
     def get_priv_pass(self):
         return self.priv_pass
 
-# examles of using easysnmp modlue https://easysnmp.readthedocs.io/en/latest/
 
-env_vars = EnvVars()
-# Create an SNMP session to be used for all our requests
-session = Session(hostname = 'snmpd:1662', version = 3, 
+class Client(threading.Thread):
+
+    def __init__(self, env_vars, msg):
+        threading.Thread.__init__(self)
+        self.env_vars = env_vars
+        self.msg = msg
+        self.json_string = ''
+        self.session = Session(version = 3, 
                   security_level = 'auth_with_privacy', security_username = 'mynewuser', 
-                  privacy_protocol = 'AES', privacy_password = env_vars.get_priv_pass(),
-                  auth_protocol = 'SHA', auth_password = env_vars.get_auth_pass())
+                  privacy_protocol = 'AES', privacy_password = self.env_vars.get_priv_pass(),
+                  auth_protocol = 'SHA', auth_password = self.env_vars.get_auth_pass())
 
-# You may retrieve an individual OID using an SNMP GET
-myIntMibObject = session.get('1.3.6.1.4.1.8073.1.1.4')
-print ('{oid} | {oid_index} | {snmp_type}  | {value}'.format(
-        oid=myIntMibObject.oid,
-        oid_index=myIntMibObject.oid_index,
-        snmp_type=myIntMibObject.snmp_type,
-        value=myIntMibObject.value
-    ))
+    def msg_to_json(self):
+        request_json = json.loads(self.msg)
+        if request_json['method'] == 'SET':
+            self.snmp_set(request_json['oid'],
+                          request_json['value'],
+                          request_json['dataType'],
+                          request_json['destination'])
 
-# You may also specify the OID as a tuple (name, index)
-# Note: the index is specified as a string as it can be of other types than
-# just a regular integer
-'''
-contact = session.get(('sysContact', '0'))
-print ('{oid}.{oid_index} {snmp_type} = {value}'.format(
-        oid=contact.oid,
-        oid_index=contact.oid_index,
-        snmp_type=contact.snmp_type,
-        value=contact.value
-    ))
-'''
-# And of course, you may use the numeric OID too
-#description = session.get('.1.3.6.1.2.1.1.1.0')
+        value = self.snmp_get(request_json['oid'],
+                              request_json['destination'])
+        timestamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        self.json_string = ('{ "method": ' + request_json['method'] + '"' +
+                            ', "destination: ' + request_json['destination'] + '"' +
+                            ', "oid": "' + request_json['oid'] + '"' +
+                            ', "name": "' + request_json['name'] + '"' +
+                            ', "type": "' + 'snmpResponse' + '"' +
+                            ', "time": "' + timestamp + '"' +
+                            ', "hash": "' + request_json['hash'] + '"' +
+                            ', "value": "' + value + '"}')
 
-# Set a variable using an SNMP SET
-session.set('1.3.6.1.4.1.8073.1.1.5.0', 'its working just fine', 's')
-myStringMibObject = session.get('1.3.6.1.4.1.8073.1.1.5.0')
-print ('{oid} | {oid_index} | {snmp_type} | {value}'.format(
-        oid=myStringMibObject.oid,
-        oid_index=myStringMibObject.oid_index,
-        snmp_type=myStringMibObject.snmp_type,
-        value=myStringMibObject.value
-    ))
 
-# Perform an SNMP walk
-#system_items = session.walk('system')
+    def snmp_set(self, oid, value, data_type, host_name):
+        session.set(oid, value, data_type, hostname = host_name)
 
-# Each returned item can be used normally as its related type (str or int)
-# but also has several extended attributes with SNMP-specific information
-'''
-for item in system_items:
-    print ('{oid}.{oid_index} {snmp_type} = {value}'.format(
-        oid=item.oid,
-        oid_index=item.oid_index,
-        snmp_type=item.snmp_type,
-        value=item.value
-    ))
-'''
+    def snmp_get(self, host_name):
+        mib_object = session.get(oid, hostname = host_name)
+        return mib_object.value
+
+    def send(self):
+        sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+        sock.sendto(bytes(self.json_string, 'utf-8'), (self.env_vars.get_server_host, self.env_vars.get_server_port))
+
+    def run(self):
+        self.msg_to_json()
+        self.send()
+        #creating udp socket and sending the 
+
+
+def main():
+    buffer_size = 4096
+    env_vars = EnvVars()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('sam-server', '3044'))
+    while True:
+        data = s.recv(buffer_size)
+        cli = Client()
+        cli.start()
+        
+
+if __name__ == '__main__':
+    main()
+
+
